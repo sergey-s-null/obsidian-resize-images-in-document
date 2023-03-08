6b774a20302b8be7f440018cf2fa471092f6495e
Sergey Semenov <sergey.s.null@gmail.com>
2023-03-08 14:06:45 +0300

    Remove SampleModal

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ export default class MyPlugin extends Plugin {
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new Notice("Simple command");
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -54,8 +54,7 @@ export default class MyPlugin extends Plugin {
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				new Notice(`Selection: ${editor.getSelection()}`);
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
@@ -69,7 +68,6 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -93,22 +91,6 @@ export default class MyPlugin extends Plugin {
 	private extractAttachmentsPaths(fileContent: string): string[] {
 		const attachmentRegex = /!\[\[([^\[\]]*)]]/g;
 		return [...fileContent.matchAll(attachmentRegex)].map(x => x[1]);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
