c8b26b1bb6571405aee162dcf7e13a1acf067948
Sergey Semenov <sergey.s.null@gmail.com>
2023-03-09 23:05:44 +0300

    Remove useless code; Add command.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
+import { Plugin } from 'obsidian';
 import { createContainer } from "./di/createContainer";
 import { TYPES } from "./di/TYPES";
 import { PluginSettingsTab } from "./settings_tabs/PluginSettingsTab";
@@ -10,51 +10,20 @@ export default class MyPlugin extends Plugin {
 		const container = createContainer(this.app, this);
 		const pluginActions = container.get<PluginActions>(TYPES.PluginActions);
 
-		this.addRibbonIcon('dice', 'Sample Plugin', async () => {
+		this.addRibbonIcon("bug", "Debug action", async () => {
 			await pluginActions.resizeImagesInCurrentDocument();
 		});
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new Notice("Simple command");
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				new Notice(`Selection: ${editor.getSelection()}`);
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: "resize-images-in-current-document",
+			name: "Resize images in current document",
+			callback: async () => {
+				await pluginActions.resizeImagesInCurrentDocument();
 			}
 		});
 
 		const pluginSettingsTabFactory = container.get<PluginSettingsTabFactory>(TYPES.PluginSettingsTabFactory);
 		const pluginSettingsTab = pluginSettingsTabFactory() as PluginSettingsTab;
 		this.addSettingTab(pluginSettingsTab);
-	}
-
-	onunload() {
-
 	}
 }
